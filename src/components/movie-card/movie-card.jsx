efa373cae2dc812ea904c790d427daeb8c61cd5f
n-townsend <132301374+n-townsend@users.noreply.github.com>
2023-08-02 12:38:57 -0400

    formatting

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,23 +1,28 @@
-import PropTypes from "prop-types";
+import proptypes from "prop-types";
+import { Card, CardImg } from "react-bootstrap";
+import "./movie-card.scss";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
+    <Card className="h-100"
       onClick={() => {
         onMovieClick(movie);
       }}
-    >
-      {movie.title}
-    </div>
+      >
+      <CardImg src={movie.image} width={150} height={180} alt="ImgMovie"></CardImg>
+      <Card.Body>
+      <Card.Title>{movie.title}</Card.Title>
+      </Card.Body>
+    </Card>
   );
 };
 
-MovieCard.PropTypes = {
-  movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    image: PropTypes.string,
-    director: PropTypes.string,
-    genre: PropTypes.string
+MovieCard.proptypes = {
+  movie: proptypes.shape({
+    title: proptypes.string.isRequired,
+    image: proptypes.string,
+    director: proptypes.string,
+    genre: proptypes.string
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  onMovieClick: proptypes.func.isRequired
 };
